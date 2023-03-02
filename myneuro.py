3a1b4ee512eaa74d37df771d9125d0fa21a92d66
naddamuhamed <79428643+naddamuhamed@users.noreply.github.com>
2023-03-02 23:10:48 +0200

    added realtime classification using saved models

--- myneuro.py
+++ myneuro.py
@@ -9,7 +9,7 @@ from threading import Thread
 import pandas as pd
 import time
 import recordingCSV
-import classifyAttention
+import classify
 
 # start=time.process_time();
 class PyNeuro:
@@ -113,7 +113,7 @@ class PyNeuro:
     def __packetParser(self):
         try:
             while True:
-                while len(self.__delta_records)!=60:
+                while len(self.__delta_records)!=30:
 
                     line = self.__telnet.read_until(b'\r');
                     if len(line) > 20:
@@ -170,17 +170,11 @@ class PyNeuro:
                                     self.__blinkStrength_records.append(data["blinkStrength"])
                         except:
                             print()
-                # print(self.__attention_records)
-                # print(self.__delta_records)
-                # print(self.__theta_records)
-                # df = pd.DataFrame({"attention" : self.__attention_records, "meditation" : self.__meditation_records,"delta" : self.__delta_records, "theta" : self.__theta_records
-                #                    , "lowAlpha" : self.__lowAlpha_records, "highAlpha" : self.__highAlpha_records, "lowBeta" : self.__lowBeta_records,
-                #                    "highBeta" : self.__highBeta_records, "lowGamma" : self.__lowGamma_records, "highGamma" : self.__highGamma_records})
-                # df.to_csv("meawake.csv", index=False)
-                classAtt=classifyAttention.att(self.__attention_records)
+          
+                classification=classify.classfy(self.__lowAlpha_records,self.__highAlpha_records)
                 recordingCSV.savetoCSV(self.__attention_records,self.__meditation_records,self.__delta_records,self.__theta_records,self.__lowAlpha_records,
                                     self.__highAlpha_records,self.__lowBeta_records,self.__highBeta_records,self.__lowGamma_records,self.__highGamma_records
-                                    ,classAtt,"anything")
+                                    ,classification,"anything6")
                 self.__attention_records = []
                 self.__meditation_records = []
                 self.__blinkStrength_records = []
