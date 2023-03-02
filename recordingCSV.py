194beec6e8bfc9138cc887dd79b674378aa9778f
naddamuhamed <79428643+naddamuhamed@users.noreply.github.com>
2023-03-02 23:11:40 +0200

    added the obtained classification to the csv file

--- recordingCSV.py
+++ recordingCSV.py
@@ -1,12 +1,17 @@
 import pandas as pd
+import numpy as np
 
-def savetoCSV(attention,meditation,delta,theta,lowAlpha,highAlpha,lowBeta,highBeta,lowGamma,highGamma,filename):
+def savetoCSV(attention,meditation,delta,theta,lowAlpha,highAlpha,lowBeta,highBeta,lowGamma,highGamma,classification,filename):
 
     try: 
+        if classification==0:
+            classification=np.zeros(len(attention), dtype=int)
+        else:
+            classification=np.ones(len(attention), dtype=int)
         dataset = pd.read_csv(filename+'.csv')
         df = pd.DataFrame({"attention" : attention, "meditation" : meditation,"delta" : delta, "theta" : theta
                                     , "lowAlpha" : lowAlpha, "highAlpha" : highAlpha, "lowBeta" : lowBeta,
-                                    "highBeta" : highBeta, "lowGamma" : lowGamma, "highGamma" : highGamma})
+                                    "highBeta" : highBeta, "lowGamma" : lowGamma, "highGamma" : highGamma,"classification" : classification})
         dataset=pd.concat([dataset,df])
         dataset.to_csv(filename+'.csv',index=False)  
         
@@ -14,6 +19,6 @@ def savetoCSV(attention,meditation,delta,theta,lowAlpha,highAlpha,lowBeta,highBe
     except FileNotFoundError:
         df = pd.DataFrame({"attention" : attention, "meditation" : meditation,"delta" : delta, "theta" : theta
                                     , "lowAlpha" : lowAlpha, "highAlpha" : highAlpha, "lowBeta" : lowBeta,
-                                    "highBeta" : highBeta, "lowGamma" : lowGamma, "highGamma" : highGamma})
+                                    "highBeta" : highBeta, "lowGamma" : lowGamma, "highGamma" : highGamma, "classification" : classification})
         df.to_csv(filename+'.csv', index=False)
 
